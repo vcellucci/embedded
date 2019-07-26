7269ecaf6cdd380fddc47743d9ccf9c6311ba917
Vittorio Cellucci <vittorio.cellucci@qlik.com>
2019-07-26 07:13:20 -0400

    better code generation

--- busywaitcpp/main.cpp
+++ busywaitcpp/main.cpp
@@ -1,46 +1,42 @@
-// This is super useless code that cycles an LED on a TivaC board in modern c++
 
-template<unsigned int T>
-class register_t {
-public:
-    register_t() = default;
-    register_t(unsigned int val){
-      *((unsigned int*)T) = val;
-    }
-      
-    void operator = (unsigned int val) {
-      *((unsigned int*)T) = val;
-    }
+namespace gpiof {
+    static constexpr unsigned int direction     = 0x40025400U;
+    static constexpr unsigned int clock_gate    = 0x400FE608U;
+    static constexpr unsigned int digital_mode  = 0x4002551CU;
+    static constexpr unsigned int data          = 0x400253FCU;
 };
 
-using Direction_Mode = register_t<0x40025400U>;
-using Clock_Gate = register_t<0x400FE608U>;
-using LED_Pin = register_t<0x400253FCU>;
-using Digital_Mode = register_t<0x4002551CU>;
+template<unsigned int address, unsigned int val>
+class register_t {
+public:
+  static void set() {
+    *reinterpret_cast<volatile unsigned int*>(address) = val;
+  }
+};
 
 
 int main(){
-    Clock_Gate( 0x20U ); // enable GPIO F
-    Direction_Mode(0X0Eu); // set direction to output
-    Digital_Mode( 0X0Eu ); // set to digital bode
+  register_t<gpiof::clock_gate, 0x20U>::set(); // enable gpiof
+  register_t<gpiof::direction, 0x0EU>::set();  // set all to output
+  register_t<gpiof::digital_mode, 0x0EU>::set(); // set all to digital
+  using blue_led = register_t<gpiof::data, 0x4U>;
+  using red_led  = register_t<gpiof::data, 0x2U>;
 
-    auto busyWait = [] {
+  auto busyWait = [] {
     int counter = 0;
       while(counter < 1000000 ){
         counter ++;
       }
     };
-    LED_Pin led;
 
     while(1){
-      // 0x3FC is all bits, ie 0xFF*4
-      led = 0X04u; // blue
+      blue_led::set();
       busyWait();
 
-      led = 0X2u; // red
+      red_led::set();
       busyWait();
 
     }
-
+  
     return 0;
 }

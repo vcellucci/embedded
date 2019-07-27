989ed7cf9462327d3e3b74318a7d073e929cc074
Vittorio Cellucci <vittorio.cellucci@qlik.com>
2019-07-26 20:58:31 -0400

    MOdified register_t class

--- busywaitcpp/main.cpp
+++ busywaitcpp/main.cpp
@@ -1,20 +1,48 @@
 
-namespace gpiof {
-    static constexpr unsigned int direction     = 0x40025400U;
-    static constexpr unsigned int clock_gate    = 0x400FE608U;
-    static constexpr unsigned int digital_mode  = 0x4002551CU;
-    static constexpr unsigned int data          = 0x400253FCU;
-};
-
 using uint32_t = unsigned int;
 using int32_t  = int;
 
-template<typename address_type, address_type address, address_type val>
+
+namespace gpiof {
+    static constexpr uint32_t direction     = 0x40025400U;
+    static constexpr uint32_t clock_gate    = 0x400FE608U;
+    static constexpr uint32_t digital_mode  = 0x4002551CU;
+    static constexpr uint32_t data          = 0x40025000U;
+    static constexpr uint32_t red_led       = 0x02U;
+    static constexpr uint32_t blue_led      = 0x04U;
+    static constexpr uint32_t green_led     = 0x08U;
+
+};
+
+template<typename address_type,
+        address_type address,
+        address_type val>
 class register_t {
 public:
-    static void set() {
+    static void write() {
         *reinterpret_cast<volatile address_type*>(address) = val;
     }
+
+    static void set() {
+        // The two here, is because the MCU does not use the first 2 bits, must be a factor of 4
+        (*reinterpret_cast<volatile address_type*>(address + (val << 2))) = val;
+    }
+
+    static void zero() {
+        // The two here, is because the MCU does not use the first 2 bits, must be a factor of 4
+        (*reinterpret_cast<volatile address_type*>(address + (val << 2))) = 0;
+    }
+
+    // returns value for direct access
+    static address_type& value() {
+        return *reinterpret_cast<volatile address_type*>(address);
+    }
+
+    // returns value for read only
+    static const address_type& const_value() {
+        return *reinterpret_cast<volatile address_type*>(address);
+    }
+
 };
 
 template<typename counter_type, counter_type value>
@@ -29,22 +57,27 @@ public:
 };
 
 
+
 int main(){
 
-    register_t<uint32_t, gpiof::clock_gate, 0x20U>::set(); // enable gpiof
-    register_t<uint32_t, gpiof::direction, 0x0EU>::set();  // set all to output
-    register_t<uint32_t, gpiof::digital_mode, 0x0EU>::set(); // set all to digital
-    using blue_led = register_t<uint32_t, gpiof::data, 0x4U>;
-    using red_led  = register_t<uint32_t, gpiof::data, 0x2U>;
-    using busy     = busy_wait_t<int32_t, 1000000>;
+    register_t<uint32_t, gpiof::clock_gate, 0x20U>::write(); // enable gpiof
+    register_t<uint32_t, gpiof::direction, 0x0EU>::write();  // set all to output
+    register_t<uint32_t, gpiof::digital_mode, 0x0EU>::write(); // set all to digital
 
+    using busy     = busy_wait_t<int32_t, 1000000>;
+    using red      = register_t<uint32_t, gpiof::data, gpiof::red_led>;
+    using blue     = register_t<uint32_t, gpiof::data, gpiof::blue_led>;
 
     while(1){
-        blue_led::set();
-        busy::wait();
 
-        red_led::set();
-        busy::wait();
+      red::zero();
+      blue::set();
+      busy::wait();
+
+      blue::zero();
+      red::set();
+      busy::wait();
+
     }
 
     return 0;
